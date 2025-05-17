06bd60d3030952a202b31f7bcc8f61831ce8ab19
fbettir <betti.farkas2@gmail.com>
2025-05-17 13:50:55 +0200

    deployment params

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://felhopicturepo-czbuh8f6fgazenc0.westeurope-01.azurewebsites.net/api',
 };
